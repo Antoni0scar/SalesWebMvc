ac40bf1e1bbcc15332efdfd0c345e83d10a56ec8
Antonio Oscar <antoni0scar@gmail.com>
2021-02-05 07:05:14 -0300

    Update seller and custom service exception

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Services
 {
@@ -38,6 +39,24 @@ namespace SalesWebMvc.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))//verificar se existe algum registro no BD com a condição
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+
+            catch(DbUpdateConcurrencyException e){ //capturando a exception de nível de acesso bd
+
+                throw new DBConcurrencyException(e.Message); //tratando para que seja gerado exception para camada de serviço
+            }
         }
     }
 }
